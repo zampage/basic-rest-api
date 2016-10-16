c38345bf8e59aa7326e6cf1657f2ae836d426604
Markus Chiarot <cmarkus@hotmail.ch>
2016-10-16 15:42:29 +0200

    released version 1.0 of REST class

--- REST.php
+++ REST.php
@@ -1,44 +1,108 @@
 <?php
 
+/**
+ * Class REST
+ * Allows API to extend this set of tools for parsing requests
+ *
+ * @author Markus Chiarot, zampage.com
+ *
+ */
+
 class REST
 {
 
+    /**
+     * gets the request method
+     *
+     * @return string
+     */
     function getMethod(){
         return $_SERVER['REQUEST_METHOD'];
     }
 
+    /**
+     * gets request input
+     *
+     * @return array|string
+     */
     function getInput(){
-        $input = null;
+        $input = "";
 
         switch(self::getMethod()){
             case "POST":
-                $input = self::clean($_POST);
+                $input = $this->clean($_POST);
                 break;
 
             case "GET":
-                $input = self::clean($_GET);
+                $input = $this->clean($_GET);
                 break;
 
             case "DELETE":
-                $input = self::clean($_GET);
+                $input = $this->clean($_GET);
                 break;
         }
 
         return $input;
     }
 
+    /**
+     * gets response code from codetable
+     *
+     * @param int $num response code
+     * @return array
+     */
+    function getCode($num){
+        $codes = array(
+            200 => 'OK',
+            201 => 'Created',
+            204 => 'No Content',
+            404 => 'Not Found',
+            406 => 'Not Acceptable',
+            500 => 'Internal Server Error'
+        );
+        return (array_key_exists($num, $codes)) ? array($num, $codes[$num]) : array(500, $codes[500]);
+    }
+
+    /**
+     * handles a request and forwards it to function
+     */
+    function process(){
+        $func = strtolower(trim(str_replace("/","",$_REQUEST['x'])));
+        ((int)method_exists($this, $func) > 0) ? $this->$func() : $this->respond('', 404);
+    }
+
+    /**
+     * cleans request input
+     *
+     * @param array|string $input input which needs to be cleaned
+     * @return array|string
+     */
     function clean($input){
         $output = array();
 
         if(is_array($input)){
             foreach($input AS $key => $val){
-                $output[$key] = self::clean($val);
+                $output[$key] = $this->clean($val);
             }
         }else{
             $output = trim(htmlspecialchars($input));
         }
 
         return $output;
+    }
+
+    /**
+     * creates a response and sends it
+     *
+     * @param mixed $data data which will be sent
+     * @param int $code the response code
+     */
+    function respond($data, $code){
+        $code = $this->getCode($code);
+        header("HTTP/1.1 " . $code[0] . " " . $code[1]);
+        header("Content-Type:application/json");
+        echo json_encode($data);
+        exit;
     }
 
 }
