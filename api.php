5a1848012abe494ccc87b04822f0e6d52219fc7e
Markus Chiarot <cmarkus@hotmail.ch>
2016-10-16 16:01:28 +0200

    example for release 1.1

--- api.php
+++ api.php
@@ -1,23 +1,70 @@
 <?php
 
+/*
+ * import REST class
+ */
 require_once('REST.php');
 
+/**
+ * Class API
+ * lets you handle API requests
+ *
+ * @author Markus Chiarot, zampage.com
+ */
 class API extends REST
 {
 
+    /**
+     * API constructor.
+     * immediately starts to process the request
+     */
     function __construct(){
         $this->process();
     }
 
-    function customer(){
-        $c = array(
+    /**
+     * custom request function
+     */
+    function customers(){
+
+        /*
+         * data table
+         */
+        $customers = array(
             "Markus Chiarot",
             "Max Muster",
             "Yet Another Person"
         );
-        $this->respond($c, 200);
+
+        /*
+         *
+         */
+        $this->respond($customers, 200);
+
+    }
+
+    /**
+     * another custom request function
+     */
+    function password(){
+
+        /*
+         * check if a specific method is used
+         */
+        if( $this->isMethod('GET') ){
+            $this->respond('supersecretpassword', 200);
+        }
+
+        /*
+         * if the wrong method is used deny the request with code 406
+         */
+        $this->respond('You are not allowed to set/update/delete the password!', 406);
+
     }
 
 }
 
+/*
+ * init API
+ */
 new API();
